e430d9c46cd665417b1d23506e898613cc440bf9
yaya-nyan <Florian.Denker@live.de>
2022-07-15 02:12:26 +0200

    initial commit

---
+++ src/main/java/yaya/absolutecarnage/AbsoluteCarnage.java
@@ -0,0 +1,14 @@
+package yaya.absolutecarnage;
+
+import net.fabricmc.api.ModInitializer;
+
+import java.util.logging.Logger;
+
+public class AbsoluteCarnage implements ModInitializer
+{
+	@Override
+	public void onInitialize()
+	{
+	
+	}
+}
